2042a35d6b1549cc36fbf5a56c40989ce6c9c7e3
SHADAKSHARI-KV <126148168+SHADAKSHARI-KV@users.noreply.github.com>
2023-02-24 17:17:35 +0530

    Add files via upload

---
+++ proj2 app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("txt-iput");
+
+console.log(txtInput)
+
+function clickHandler() {
+    console.log("clicked!");
+};
+btnTranslate.addEventListener("click", clicktHandler)
